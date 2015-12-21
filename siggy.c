f27a02610da8c1ff94b6ea2afd3d40a4e4ddc2b4
Alyssa <alyssa@backtick.town>
2015-12-21 17:12:50 -0500

    Paint screen packet complete :-)

--- siggy.c
+++ siggy.c
@@ -5,6 +5,8 @@
 
 #include "hid.h"
 
+typedef uint16_t unsigned short;
+
 // 320x240 resolution
 
 unsigned char arr[] = {
@@ -68,8 +70,25 @@ void sendRandomPacket() {
     rawhid_send(0, &buffer, sizeof(buffer), 64);
 }
 
-void paint() {
-    rawhid_send(0, maybePaint, sizeof(maybePaint), 64);
+void paint(uint16_t xpos, uint16_t ypos, uint16_t width, uint16_t height) {
+    unsigned char buffer[11] = {
+        0xFF, 0x12, // command
+        0x01, // mode
+        0x00, 0x00, 0x00, 0x00, // poses
+        0x00, 0x00, 0x00, 0x00 // heights
+    };
+
+    // TODO: non-little endian machines
+    buffer[3] = xpos & 0xFF00;
+    buffer[4] = xpos & 0x00FF;
+    buffer[5] = ypos & 0xFF00;
+    buffer[6] = ypos & 0x00FF;
+    buffer[7] = width & 0xFF00;
+    buffer[8] = width & 0x00FF;
+    buffer[9] = height & 0xFF00;
+    buffer[10] = height & 0x00FF;
+
+    rawhid_send(0, buffer, sizeof(buffer), 64);
 }
 
 void sendTestPacket(unsigned char* buffer, int offset, int length) {
@@ -86,7 +105,9 @@ int main() {
 
     srand(time(NULL));
 
-    sendTestPacket(maybePaint, 0, 11);
+    paint(50, 50, 50, 50);
+    paint(100, 100, 20, 50);
+    paint(200, 200, 30, 40);
 
     rawhid_close(0);
 
