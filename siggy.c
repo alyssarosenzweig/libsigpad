85a7bb0d7bf79325f3ce1082247c0cb7d653b1c1
Alyssa <alyssa@backtick.town>
2015-12-20 19:54:24 -0500

    Dump as hex; discover (and docuent) a small, data prodcuing sequence

--- siggy.c
+++ siggy.c
@@ -5,17 +5,27 @@
 
 #include "hid.h"
 
+unsigned char arr[] = {
+    0xF7, 0x1C, 0x0A, 0x7E, 0xBA, 0x12, 0x8F, 0x4C,
+    0xC7, 0x13, 0x85, 0x84, 0x48, 0xBF, 0xCA, 0x70, 
+    0x71, 0xEF, 0x6D, 0xB5, 0x57, 0x63, 0xA5, 0xE9, 
+    0x1B, 0x04, 0x1F, 0x16, 0x13, 0x45, 0x09, 0x0A
+};
+
 void sendRandomPacket() {
-    char buffer[64];
+    unsigned char buffer[32];
     
     // randomly initialize buffer
     for(int i = 0; i < sizeof(buffer); ++i) {
         buffer[i] = rand() & 0xFF;
+        printf("0x%02X, ", buffer[i]);
     }
+
+    putchar('\n');
 
     //s send
 
-    rawhid_send(0, &buffer, 64, 64);
+    rawhid_send(0, &buffer, sizeof(buffer), 64);
 }
 
 int main() {
@@ -33,6 +43,7 @@ int main() {
 
     for(;;) {
         sendRandomPacket();
+        sleep(2);
     }
 
     rawhid_close(0);
