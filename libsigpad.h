4dc46abae94668b5f6dac11d86020ac4ce01bd84
Alyssa Rosenzweig <alyssa@backtick.town>
2016-04-03 18:54:56 -0400

    Refactor

---
+++ libsigpad.h
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <sys/ioctl.h>
+#include <termios.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "hid.h"
+
+#define PING_PACKET_COUNT 16
+#define TIMEOUT 4
+
+// Blits a monochrome bitmap.
+// Must fall on 8x8 boundaries
+
+void bitmap(uint16_t xpos, uint16_t ypos, uint16_t width, uint16_t height, void* data);
+
+// Lower-level tool for drawing rectangles efficiently.
+// Set mode to 2 to use it to fill a rectangle.
+
+void rectangle(uint16_t xpos, uint16_t ypos, uint16_t width, uint16_t height, uint8_t mode);
+
+// Clears display.
+void clear();
+
+// Controls backlight state. 
+// The latter two methods are for convenience only.
+
+void backlightControl(bool on);
+void backlightOn();
+void backlightOff();
