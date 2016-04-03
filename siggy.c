01612a27bd589c3bff830eab2b379c2a56a087bd
Alyssa Rosenzweig <alyssa@backtick.town>
2016-04-03 18:23:05 -0400

    Code cleanup

--- siggy.c
+++ siggy.c
@@ -3,42 +3,20 @@
 #include <memory.h>
 #include <sys/ioctl.h>
 #include <termios.h>
+#include <stdint.h>
+#include <stdbool.h>
 
 #include "hid.h"
 
 #define PING_PACKET_COUNT 16
 #define TIMEOUT 4
 
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-
 unsigned int pingTimer = PING_PACKET_COUNT;
 uint8_t pingBuffer[16];
 
-// 320x240 resolution
+// TODO: support big-endian machines
 
-/* response format document notes (hypothesis anyway)
- * not currently used, but might be useful for debugging
- *
- * first byte is always under 0x80 -- that distinguishes send/recv
- * next byte the command -- I've gotten 0x1C a couple of times
- * next few bytes I'm not sure about:
- * 0x1C 0x1D 0x26 0x04 0x1D 0x26 0x04 0x00 0x00 0x00...
- * these are *definitely* structured.. but how?
- */
-
-// HYPOTHESIS: ANY FIRST BYTE VALUE > 0x80 WORKS
-// Therefore, the first byte is a command byte.
-
-unsigned char maybeShutOff[] = {
-    0x81, 0x03
-};
-
-unsigned char maybeTurnOn[] = {
-    0x81, 0x02
-};
-
-static inline void send_packet(char* buffer, size_t length) {
+static inline void send_packet(unsigned char* buffer, size_t length) {
     rawhid_send(0, buffer, length, TIMEOUT);
 
     if(pingTimer-- == 0) {
@@ -48,50 +26,31 @@ static inline void send_packet(char* buffer, size_t length) {
     }
 }
 
-void sendBitmapRaw(uint16_t xpos, uint16_t ypos, uint16_t width, uint16_t height, void* data) {
-    printf("%d,%d x %d,%d\n", xpos, ypos, width, height);
+void bitmap(uint16_t xpos, uint16_t ypos, uint16_t width, uint16_t height, void* data) {
+    int dataLength = (width * height) >> 3,
+        bufferLength = 11 + dataLength;
 
-    unsigned char header[] = {
-        0xF2, 0x07, // command
-        0x02, // mode
-        0x00, 0x00, 0x00, 0x00, // poses
-        0x00, 0x00, 0x00, 0x00 // heights
-    };
+    unsigned char* buffer = malloc(bufferLength);
 
-    // TODO: non-little endian machines
-    header[3] = (xpos & 0xFF00) >> 8;
-    header[4] = (xpos & 0x00FF);
-    header[5] = (ypos & 0xFF00) >> 8;
-    header[6] = (ypos & 0x00FF);
-    header[7] = (width & 0xFF00) >> 8;
-    header[8] = (width & 0x00FF);
-    header[9] = (height & 0xFF00) >> 8;
-    header[10] = (height & 0x00FF);
+    buffer[0] = 0xFF; // command bytes
+    buffer[1] = 0x07;
+    buffer[2] = 0x02; // mode
+    buffer[3] = (xpos & 0xFF00) >> 8;
+    buffer[4] = (xpos & 0x00FF);
+    buffer[5] = (ypos & 0xFF00) >> 8;
+    buffer[6] = (ypos & 0x00FF);
+    buffer[7] = (width & 0xFF00) >> 8;
+    buffer[8] = (width & 0x00FF);
+    buffer[9] = (height & 0xFF00) >> 8;
+    buffer[10] = (height & 0x00FF);
 
-    int dataLength = (width * height) >> 3;
-
-    unsigned char* buffer = malloc(sizeof(header) + dataLength);
-
-    memcpy(buffer, header, sizeof(header));
-    memcpy(buffer + sizeof(header), data, dataLength);
-
-    send_packet(buffer, sizeof(header) + dataLength);
+    memcpy(buffer + 11, data, dataLength);
+    send_packet(buffer, bufferLength);
 }
 
-unsigned char maybeSetMode[] = {
-    0xFF, 0x09, // (guess) command to set mode
-    0x02 // mode
-};
+void rectangle(uint16_t xpos, uint16_t ypos, uint16_t width, uint16_t height, uint8_t mode) {
+    unsigned char buffer[11] = { 0xFF, 0x12, mode };
 
-void paint(uint16_t xpos, uint16_t ypos, uint16_t width, uint16_t height, uint8_t mode) {
-    unsigned char buffer[11] = {
-        0xFF, 0x12, // command
-        mode, // mode
-        0x00, 0x00, 0x00, 0x00, // poses
-        0x00, 0x00, 0x00, 0x00 // heights
-    };
-
-    // TODO: non-little endian machines
     buffer[3] = (xpos & 0xFF00) >> 8;
     buffer[4] = (xpos & 0x00FF);
     buffer[5] = (ypos & 0xFF00) >> 8;
@@ -105,33 +64,34 @@ void paint(uint16_t xpos, uint16_t ypos, uint16_t width, uint16_t height, uint8_
 }
 
 void clear() { 
-    paint(0, 0, 320, 240, 2);
+    rectangle(0, 0, 320, 240, 2);
     usleep(1000000);
 }
 
-void hexdump(unsigned char* buffer, size_t length) {
-    for(int i = 0; i < length; ++i) {
-        printf("%X ", buffer[i]);
-    }
-    putchar('\n');
+void backlightControl(bool on) {
+    unsigned char buffer[] = { 0x81, 0x02 | (!on) };
+    send_packet(buffer, 2);
 }
 
-int main() {
-    int r = rawhid_open(10, 0x06A8, 0x0043, -1, -1);
+void backlightOn() { backlightControl(1); }
+void backlightOff() { backlightControl(0); }
 
-    if(r != 1) {
-        printf("Ahh! signature pad thing not working, hm: %d\n", r);
+int main() {
+    int status = rawhid_open(10, 0x06A8, 0x0043, -1, -1); 
+
+    if(status != 1) {
+        printf("Opening error: %d\n", status);
         return -1;
     }
 
     clear();
 
+    // test program -- fill screen with A's
     char charA[] = { 0x18, 0x3C, 0x66, 0x7E, 0x66, 0x66, 0x00, 0x00 };
-    char buffer[8];
-    
+
     for(int y = 0; y < 30; ++y) {
         for(int x = 0; x < 40; ++x) {
-            sendBitmapRaw(x << 3, y << 3, 8, 8, charA);
+            bitmap(x << 3, y << 3, 8, 8, charA);
         }
    }
 
