763871c0ff74a901f4bc0f4b43649f0a8b335881
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2017-02-18 17:28:42 -0800

    LSB delta's

--- stext.c
+++ stext.c
@@ -14,7 +14,7 @@ void initializeGlyphs() {
 void renderString(char* str, int x, int y, int size) {
     FT_Set_Pixel_Sizes(face, 0, size);
     
-    int prev = 0;
+    int prev = 0, prevDX = 0;
 
     while(*str != '\0') {
         int glyph = FT_Get_Char_Index(face, *str++);
@@ -28,7 +28,15 @@ void renderString(char* str, int x, int y, int size) {
         FT_Load_Glyph(face, glyph, FT_LOAD_RENDER | FT_LOAD_MONOCHROME);
         FT_Bitmap bmp = face->glyph->bitmap;
 
-        bitmap(x + face->glyph->bitmap_left, y + size - face->glyph->bitmap_top, ((bmp.width + 15) >> 4 << 4) , bmp.rows, bmp.buffer);
+        if(prevDX - face->glyph->lsb_delta >= 32) {
+            x--;
+        } else if(prevDX - face->glyph->lsb_delta < -32) {
+            x++;
+        }
+
+        prevDX = face->glyph->rsb_delta;
+
+        bitmap(x + face->glyph->lsb_delta + face->glyph->bitmap_left, y + size - face->glyph->bitmap_top, ((bmp.width + 15) >> 4 << 4) , bmp.rows, bmp.buffer);
 
         x += face->glyph->advance.x >> 6;
         y += face->glyph->advance.y >> 6;
