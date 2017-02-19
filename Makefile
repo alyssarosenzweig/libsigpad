36286d46765df926b24551b92bae12a1fa07325a
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2017-02-18 16:18:04 -0800

    kerning

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 all: hid.o
-	cc -o clock clock.c hid.o libsigpad.c stext.c -std=c99 `freetype-config --cflags` `freetype-config --libs` -lusb -lpthread
+	cc -o cat cat.c hid.o libsigpad.c stext.c -std=c99 `freetype-config --cflags` `freetype-config --libs` -lusb -lpthread
 
 hid.o: hid_LINUX.c hid.h
 	cc $(CFLAGS) -c -o $@ $<
 
 clean:
-	rm -f *.o clock
+	rm -f *.o cat
 	rm -rf tmp
 
--- stext.c
+++ stext.c
@@ -8,19 +8,31 @@ FT_Face face;
 
 void initializeGlyphs() {
     if(FT_Init_FreeType(&ft)) printf("ft init error\n");
-    if(FT_New_Face(ft, "/usr/share/fonts/truetype/liberation/LiberationMono-Regular.ttf", 0, &face)) printf("ft glyph error\n");
+    if(FT_New_Face(ft, "/usr/share/fonts/truetype/liberation/LiberationSans-Regular.ttf", 0, &face)) printf("ft glyph error\n");
 }
 
 void renderString(char* str, int x, int y, int size) {
     FT_Set_Pixel_Sizes(face, 0, size);
     
+    int prev = 0;
+
     while(*str != '\0') {
-        FT_Load_Char(face, *str++, FT_LOAD_RENDER | FT_LOAD_MONOCHROME);
+        int glyph = FT_Get_Char_Index(face, *str++);
+
+        if(FT_HAS_KERNING(face) && glyph) {
+            FT_Vector kerning;
+            FT_Get_Kerning(face, prev, glyph, FT_KERNING_DEFAULT, &kerning);
+            x += kerning.x >> 6;
+        }
+
+        FT_Load_Glyph(face, glyph, FT_LOAD_RENDER | FT_LOAD_MONOCHROME);
         FT_Bitmap bmp = face->glyph->bitmap;
 
         bitmap(x, y, ((bmp.width + 15) >> 4 << 4) , bmp.rows, bmp.buffer);
 
         x += face->glyph->advance.x >> 6;
         y += face->glyph->advance.y >> 6;
+
+        prev = glyph;
     }
 }
