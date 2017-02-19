16b2a992573034be8638e98170b393b02633a423
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2017-02-18 20:26:33 -0800

    Cat sample

---
+++ cat.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "stext.h"
+
+void main() {
+    if(init_sigpad()) return -1;
+    initializeGlyphs();
+
+    char buffer[40];
+    
+    int cursorX = 8, cursorY = 8;
+
+    while(fgets(buffer, sizeof(buffer), stdin)) {
+        if(cursorY > 240) {
+            clear();
+            cursorY = 8;
+        }
+        
+        buffer[strlen(buffer) - 1] = '\0';
+        renderString(buffer, cursorX, cursorY, 12);
+
+        cursorY += 12;
+    }
+}
